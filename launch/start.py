4f19dd392423322deb534275e6df31bab0a4c493
Shrijit Singh <shrijitsingh99@gmail.com>
2024-04-04 20:06:03 -0400

    Cleanup launch and param file

--- launch/start.py
+++ launch/start.py
@@ -1,12 +1,25 @@
 from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    ld = LaunchDescription()
+    current_pkg = FindPackageShare('hesai_ros_driver')
 
-    rviz_config=get_package_share_directory('hesai_ros_driver')+'/rviz/rviz2.rviz'
+    rviz_config = PathJoinSubstitution([current_pkg, 'rviz', 'rviz2.rviz'])
 
-    return LaunchDescription([
-        Node(namespace='hesai_ros_driver', package='hesai_ros_driver', executable='hesai_ros_driver_node', output='screen'),
-        Node(namespace='rviz2', package='rviz2', executable='rviz2', arguments=['-d',rviz_config])
-    ])
+    hesai_node = Node(
+            namespace='hesai_ros_driver',
+            package='hesai_ros_driver',
+            executable='hesai_ros_driver_node',
+            output='screen',
+    )
+    ld.add_action(hesai_node)
+    
+    rviz_node = Node(namespace='rviz2', package='rviz2', executable='rviz2', arguments=['-d', rviz_config])
+    ld.add_action(rviz_node)
+
+    return ld
