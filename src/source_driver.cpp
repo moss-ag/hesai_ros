d9923fd4ca1b91d6fb4901680fe5bb031ffaf7f2
Karan Khanna <35415383+Glonks@users.noreply.github.com>
2024-10-23 04:04:34 +0530

    add diagnostic publishers and activation/deactivation functionality (#1)
    * added diagnostic publishers
    added service to set state
    
    * fixed erroneously named setup_toggle_state_service
    
    * added setting of response data
    
    * fixed creation of callback
    
    * Switch to diagnostics for LiDAR status
    
    ---------
    
    Co-authored-by: Shrijit Singh <shrijitsingh99@gmail.com>

--- src/source_driver.cpp
+++ src/source_driver.cpp
@@ -9,12 +9,16 @@ namespace hesai_ros
 
 SourceDriver::SourceDriver(): 
 node_(std::make_shared<rclcpp::Node>("hesai_node")),
-lidar_driver_(std::make_shared<HesaiLidarSdk<LidarPointXYZIRT>>())
+lidar_driver_(std::make_shared<HesaiLidarSdk<LidarPointXYZIRT>>()),
+diagnostic_updater_(node_, 5.0)
 {
+  diagnostic_updater_.setHardwareID("Hesai LiDAR XT32");
+
   param_listener_ = std::make_shared<hesai_ros::ParamListener>(
   node_->get_node_parameters_interface());
   params_ = param_listener_->get_params();
 
+  setup_set_state_service();
   setup_packet_publisher();
   setup_pointcloud_publisher();
   setup_status_publisher();
@@ -88,6 +92,13 @@ DriverParam SourceDriver::set_params()
   return driver_param;
 }
 
+void SourceDriver::setup_set_state_service() {
+  set_state_service_ = node_->create_service<std_srvs::srv::SetBool>(
+    "~/activate",
+    std::bind(&SourceDriver::set_state_callback, this, std::placeholders::_1, std::placeholders::_2)
+  );
+}
+
 void SourceDriver::setup_pointcloud_publisher() {
   if (!params_.publish_pointcloud)
   {
@@ -95,6 +106,12 @@ void SourceDriver::setup_pointcloud_publisher() {
   }
 
   cloud_pub_ = node_->create_publisher<sensor_msgs::msg::PointCloud2>(params_.topics.pointcloud, 100);
+
+  diagnosed_cloud_pub_ = std::make_shared<diagnostic_updater::DiagnosedPublisher<sensor_msgs::msg::PointCloud2>>(
+    cloud_pub_,
+    diagnostic_updater_,
+    diagnostic_updater::FrequencyStatusParam(&params_.diagnostics.cloud_min_freq, &params_.diagnostics.cloud_max_freq, 0, 10),
+    diagnostic_updater::TimeStampStatusParam(-1, 1));
 
   // Reserve 50K points
   pcl_ros_msg_.data.reserve(sizeof(LidarPointXYZIRT) * 5e4);
@@ -142,6 +159,12 @@ void SourceDriver::setup_packet_publisher()
       params_.topics.packet,
       10);
 
+    diagnosed_pkt_pub_ = std::make_shared<diagnostic_updater::DiagnosedPublisher<hesai_ros_driver::msg::UdpFrame>>(
+      pkt_pub_,
+      diagnostic_updater_,
+      diagnostic_updater::FrequencyStatusParam(&params_.diagnostics.pkt_min_freq, &params_.diagnostics.pkt_max_freq, 0, 10),
+      diagnostic_updater::TimeStampStatusParam(-1, 1));
+
     udp_frame_ros_msg_.packets.reserve(1000);
     udp_frame_ros_msg_.header.frame_id = params_.frame_id;
 
@@ -149,20 +172,6 @@ void SourceDriver::setup_packet_publisher()
       std::bind(
         &SourceDriver::publish_packet, this, std::placeholders::_1,
         std::placeholders::_2));
-  }
-}
-
-void SourceDriver::setup_status_publisher()
-{
-  if (params_.publish_status) {
-    status_pub_ = node_->create_publisher<hesai_ros_driver::msg::LidarStatus>(
-      params_.topics.status,
-      10);
-
-    lidar_driver_->RegRecvCallback(
-      std::bind(
-        &SourceDriver::publish_lidar_status, this,
-        std::placeholders::_1));
   }
 }
 
@@ -200,8 +209,8 @@ void SourceDriver::publish_pointcloud(const LidarDecodedFrame<LidarPointXYZIRT> 
   pcl_ros_msg_.header.stamp.sec = (uint32_t) floor(frame.points[0].timestamp);
   pcl_ros_msg_.header.stamp.nanosec =
     (uint32_t) round((frame.points[0].timestamp - pcl_ros_msg_.header.stamp.sec) * 1e9);
-  
-  cloud_pub_->publish(pcl_ros_msg_);
+
+  diagnosed_cloud_pub_->publish(pcl_ros_msg_);
 }
 
 void SourceDriver::publish_packet(const UdpFrame_t & msg, double timestamp)
@@ -220,7 +229,7 @@ void SourceDriver::publish_packet(const UdpFrame_t & msg, double timestamp)
   udp_frame_ros_msg_.header.stamp.nanosec = (uint32_t)round(
     (timestamp - udp_frame_ros_msg_.header.stamp.sec) * 1e9);
 
-  pkt_pub_->publish(udp_frame_ros_msg_);
+  diagnosed_pkt_pub_->publish(udp_frame_ros_msg_);
 }
 
 void SourceDriver::packet_callback(const hesai_ros_driver::msg::UdpFrame::SharedPtr msg)
@@ -232,22 +241,97 @@ void SourceDriver::packet_callback(const hesai_ros_driver::msg::UdpFrame::Shared
   }
 }
 
-void SourceDriver::publish_lidar_status(const hesai::lidar::LidarStatus & status)
+void SourceDriver::set_state_callback(
+  const std::shared_ptr<std_srvs::srv::SetBool::Request> request,
+  std::shared_ptr<std_srvs::srv::SetBool::Response> response
+)
 {
-  hesai_ros_driver::msg::LidarStatus status_msg;
-  status_msg.system_uptime = status.system_uptime;
-  status_msg.motor_speed = status.motor_speed;
-  for (int i = 0; i < 8; ++i) {
-    status_msg.temperature[i] = status.temperature[i] / 100.0;
-  }
-  status_msg.gps_pps_lock = status.gps_pps_lock;
-  status_msg.gps_gprmc_status = status.gps_gprmc_status;
-  status_msg.startup_times = status.startup_times;
-  status_msg.total_operation_time = status.total_operation_time;
-  status_msg.ptp_status = status.ptp_status;
-  status_msg.ptp_offset = status.ptp_offset;
+  if (request->data) {
+    lidar_driver_->lidar_ptr_->ptc_client_->SetStandbyMode(0);
 
-  status_pub_->publish(status_msg);
+    RCLCPP_INFO(node_->get_logger(), "Lidar driver activated");
+  } else {
+    lidar_driver_->lidar_ptr_->ptc_client_->SetStandbyMode(1);
+
+    RCLCPP_INFO(node_->get_logger(), "Lidar driver deactivated");
+  }
+
+  response->success = true;
+}
+
+
+void SourceDriver::setup_status_publisher()
+{
+  if (params_.publish_status) {
+    status_task_ = std::make_shared<diagnostic_updater::FunctionDiagnosticTask>(
+      "lidar status", [&] (diagnostic_updater::DiagnosticStatusWrapper & stat)
+      {
+        if (lidar_driver_->lidar_ptr_ == nullptr || lidar_driver_->lidar_ptr_->ptc_client_ == nullptr) {
+          stat.summary(diagnostic_msgs::msg::DiagnosticStatus::ERROR, "LiDAR is not running");
+          return;
+        }
+
+        hesai::lidar::LidarStatus lidar_status;
+        int ret = lidar_driver_->lidar_ptr_->ptc_client_->GetLidarStatus(lidar_status);
+        if (ret == 0) {
+          stat.summary(diagnostic_msgs::msg::DiagnosticStatus::OK, "LiDAR is running");
+          stat.add("System uptime (s)", lidar_status.system_uptime);
+          stat.add("Motor speed (RPM)", lidar_status.motor_speed);
+          stat.add("Bottom circuit board T1 (°C)", lidar_status.temperature[0] / 100.0);
+          stat.add("Bottom circuit board T2 (°C)", lidar_status.temperature[1] / 100.0);
+          stat.add("Laser emitting board RT_L1 (°C)", lidar_status.temperature[2] / 100.0);
+          stat.add("Laser emitting board RT_L2 (°C)", lidar_status.temperature[3] / 100.0);
+          stat.add("Laser Receiving board RT_R (°C)", lidar_status.temperature[4] / 100.0);
+          stat.add("Laser Receiving board RT2 (°C)", lidar_status.temperature[5] / 100.0);
+          stat.add("Top circuit RT3 (°C)", lidar_status.temperature[6] / 100.0);
+          stat.add("Top circuit RT4 (°C)", lidar_status.temperature[7] / 100.0);
+          stat.add("GPS PPS status", lidar_status.gps_pps_lock ? "Locked" : "Unlocked");
+          stat.add("GPS NMEA status", lidar_status.gps_gprmc_status ? "Locked" : "Unlocked");
+          stat.add("System start-up times", lidar_status.startup_times);
+          stat.add("Total time in operation", lidar_status.total_operation_time);
+
+          static const std::unordered_map<uint8_t, std::string> ptp_status_map = {
+            {0, "Free run"},
+            {1, "Tracking"},
+            {2, "Locked"},
+            {3, "Frozen"},
+          };
+          std::string ptp_status_str = "Unknown";
+          if (ptp_status_map.find(lidar_status.ptp_status) != ptp_status_map.end()) {
+            ptp_status_str = ptp_status_map.at(lidar_status.ptp_status);
+          }
+          stat.add("PTP status", ptp_status_str);
+  
+          stat.add("PTP offset (ns)", lidar_status.ptp_offset);
+          
+          static const std::unordered_map<uint8_t, std::string> ptp_state_map = {
+            {0, "None"},
+            {1, "Intializing"},
+            {2, "Faulty"},
+            {3, "Disabled"},
+            {4, "Listening"},
+            {5, "Premaster"},
+            {6, "Master"},
+            {7, "Passive"},
+            {8, "Uncalibrated"},
+            {9, "Slave"},
+            {10, "Grandmaster"},
+          };
+          std::string ptp_state_str = "Unknown";
+          if (ptp_state_map.find(lidar_status.ptp_state) != ptp_state_map.end()) {
+            ptp_state_str = ptp_state_map.at(lidar_status.ptp_state);
+          }
+          stat.add("PTP state", ptp_state_str);
+
+          stat.add("PTP handshake elapsed time (ms)", lidar_status.ptp_handshake_elapsed_time);
+
+        } else {
+          stat.summary(diagnostic_msgs::msg::DiagnosticStatus::ERROR, "Failed to get lidar status");
+        }
+
+      });
+    diagnostic_updater_.add(*status_task_);
+  }
 }
 
 } // namespace hesai_ros
